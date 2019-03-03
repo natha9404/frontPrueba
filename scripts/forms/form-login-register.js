706325cb6cdc1b39590ec09d4f452642e59847c3
natha9404 <natha9404@gmail.com>
2019-03-03 17:13:59 -0500

    inicio

---
+++ scripts/forms/form-login-register.js
@@ -0,0 +1,53 @@
+const serviceUrl = 'http://127.0.0.1:8000/';
+let token = '';
+
+$('#login-btn').on('click', function (e) {
+	/* Button login */
+	e.preventDefault();
+	login();
+});
+
+function login() {
+	userData = {
+		'username': $('#user-name').val(),
+		'password': $("#user-password").val()
+	}
+	$.ajax({
+		url: serviceUrl + 'usuarios/login/',
+		type: 'POST',
+		data: userData,
+		success: function (result) {
+            console.log(result.token)
+            let cookies = { token: result.token};
+            console.log('paso');
+			if (localStorage.pruebaCookies) {
+				localStorage.pruebaCookies = JSON.stringify(cookies);
+			} else {
+				localStorage.setItem('pruebaCookies', JSON.stringify(cookies));
+            }
+            window.location.replace("site/index.html");
+			
+		},
+		error: function (error) {
+			var cookies = { token: '', role: '' };
+			localStorage.setItem('pruebaCookies', JSON.stringify(cookies));
+            console.log(error);
+            console.log('HOLAAAA');
+            document.getElementById('errorname').innerHTML ="Credenciales incorrectas, intente nuevamente.";
+            document.getElementById('errorname').style.display="block"
+			$('#user-name').val('');
+			$("#user-password").val('');
+		}
+	});
+}
+
+/*
+$(document).ready ( function(){
+	console.log('>>>>>>>>>>>>>>>>>>>>>>>> object',localStorage.linceCookie);
+	if (localStorage.pruebaCookies) {
+		if(JSON.parse(localStorage.pruebaCookies).token != ""){
+			//window.location.replace("site/");
+		}
+
+	} 
+ });*/
